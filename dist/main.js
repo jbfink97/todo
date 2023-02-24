3d19ae46b9896f95c8e98f9b48e1dff06d09413b
Johnny <johnnyfink97@gmail.com>
2023-02-24 17:25:50 -0500

    set up barebones html, package.json, config, JS, etc. Made sure webpack is working

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const t=document.getElementById("content"),e=document.createElement("div");e.textContent="this is a test",t.appendChild(e),console.log("another test")})();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const body = document.getElementById('content');
+const test = document.createElement('div');
+test.textContent = 'this is a test';
+body.appendChild(test);
+
+console.log('another test');
