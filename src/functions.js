49adacbbfb9383ef409948a7e497160f262039db
Johnny <johnnyfink97@gmail.com>
2023-03-26 21:41:55 -0400

    worked on function to add individual tasks to projects

--- src/functions.js
+++ src/functions.js
@@ -46,7 +46,9 @@ function renderProjects() {
         deleteProjectButton.addEventListener('click', () => {
             projects.splice(project.index, 1);
             renderProjects();
-            if (document.getElementById('currentProject').classList.contains(project.name)) {
+            if (projects.length < 1) {
+                renderDisplay('');
+            } else if (document.getElementById('currentProject').classList.contains(project.name)) {
                 renderDisplay(projects[0].name);
             }
             //showAll();
@@ -64,6 +66,134 @@ function renderDisplay(name) {
     currentProject.textContent = `Project: ${name}`;
     currentProject.classList = name;
     projectDisplay.append(currentProject);
+
+    const taskList = document.createElement('div');
+    taskList.id = 'taskList';
+    projectDisplay.append(taskList);
+    updateTasks();
+
+    // create new task form
+    const newTaskForm = document.createElement('form');
+    newTaskForm.id = 'newTaskForm';
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'description';
+    input.placeholder = 'Task to add';
+    const inputLabel = document.createElement('label');
+    inputLabel.textContent = 'Name/description of task';
+    const inputDiv = document.createElement('div');
+    inputDiv.append(inputLabel, input);
+
+
+    const dateInput = document.createElement('input');
+    dateInput.type = 'date';
+    dateInput.id = 'dueDate';
+    const dateLabel = document.createElement('label');
+    dateLabel.textContent = 'Due date';
+    const dateDiv = document.createElement('div');
+    dateDiv.append(dateLabel, dateInput);
+
+    const priorityLevels = ['high', 'medium', 'low'];
+    const select = document.createElement('select');
+    for (const level of priorityLevels) {
+        const option = document.createElement('option');
+        option.value = level;
+        option.text = level.charAt(0).toUpperCase() + level.slice(1);
+        select.appendChild(option);
+    }
+    select.id = 'priority';
+
+    const label = document.createElement('label');
+    label.htmlFor = 'priority';
+    label.textContent = 'Priority level';
+    const priorityDiv = document.createElement('div');
+    priorityDiv.append(label, select);
+
+    // add task to project button
+    const newTaskBtn = document.createElement('button');
+    newTaskBtn.type = 'button';
+    newTaskBtn.classList.add('button');
+    newTaskBtn.textContent = 'Add task to project';
+    newTaskBtn.addEventListener('click', addTask);
+
+    newTaskForm.append(inputDiv, dateDiv, priorityDiv, newTaskBtn);
+    projectDisplay.append(newTaskForm);
+}
+
+function updateTasks() {
+    const taskList = document.getElementById('taskList');
+    taskList.textContent = '';
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i].index = i;
+    }
+    const project = document.getElementById('currentProject').className;
+    for (const task in tasks) {
+        if (tasks[task].project === project) {
+            taskList.append(individualTask(tasks[task]));
+        }
+    }
+}
+
+function addTask() {
+    const project = document.getElementById('currentProject').className;
+    const description = document.getElementById('description').value;
+    const dueDate = document.getElementById('dueDate').value;
+    const priority = document.getElementById('priority').value;
+    const task = new Task(project, description, dueDate, priority);
+    task.status = 'incomplete';
+    task.index = tasks.length;
+
+    if (description.length < 1) {
+        alert('Please enter a description for the task');
+    } else if (dueDate === '') {
+        alert('Please enter a due date');
+    } else {
+        tasks.push(task);
+        updateTasks();
+        document.getElementById('newTaskForm').reset();
+    }
+}
+
+function individualTask(task) {
+    const individualTaskDiv = document.createElement('div');
+    individualTaskDiv.classList.add('individualTask');
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.textContent = `Description: ${task.description}`;
+
+    const dateDiv = document.createElement('div');
+    dateDiv.textContent = `Due on: ${task.dueDate}`;
+
+    const priorityDiv = document.createElement('div');
+    priorityDiv.textContent = `Priority level: ${task.priority}`;
+
+    const completedBtn = document.createElement('button');
+    completedBtn.type = 'button';
+    completedBtn.textContent = 'Mark complete';
+    completedBtn.addEventListener('click', () => {
+        if (task.status == 'incomplete') {
+            task.status = 'complete';
+            individualTaskDiv.classList.add('complete');
+            completedBtn.textContent = 'Complete';
+        } else {
+            task.status = 'incomplete';
+            individualTaskDiv.classList.remove('complete');
+            completedBtn.textContent = 'Mark as complete';
+        }
+    })
+
+
+    const removeBtn = document.createElement('button');
+    removeBtn.type = 'button';
+    removeBtn.textContent = 'Remove task';
+    removeBtn.addEventListener('click', () => {
+        tasks.splice(task.index, 1);
+        updateTasks();
+    })
+
+    individualTaskDiv.append(descriptionDiv, dateDiv, priorityDiv, completedBtn, removeBtn);
+    return individualTaskDiv;
+
 }
 
 export {addProject, renderProjects, renderDisplay}
